8e3594aea244197cb9fbed071d58c270f35957fd
cherniatyn <v.v.4ern9tin@gmail.com>
2018-08-16 21:30:08 +0300

    install gulp, eslint. Formatting code

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,32 @@
-;(function() {
-    var cards = [];
-    var groupElement = document.getElementsByClassName('group')[0];
-    var addCardElement = groupElement.children[groupElement.children.length - 1];
+(function () {
+	var cards = [];
+	var groupElement = document.getElementsByClassName('group')[0];
+	var addCardElement = groupElement.children[groupElement.children.length - 1];
 
-    var removeCard = (event) => groupElement.removeChild(cards.splice(cards.indexOf(event.target.parentElement), 1)[0]);
+	var removeCard = function(event) {
+		return groupElement.removeChild(cards.splice(cards.indexOf(event.target.parentElement), 1)[0]);
+	}; 
 
-    var getRemoveBlock = function () {
-        var removeBlockTemplate = document.createElement("span");
-        removeBlockTemplate.className = "card-remove";
-        removeBlockTemplate.innerText = "x";
+	var getRemoveBlock = function () {
+		var removeBlockTemplate = document.createElement('span');
+		removeBlockTemplate.className = 'card-remove';
+		removeBlockTemplate.innerText = 'x';
 
-        removeBlockTemplate.addEventListener("click", removeCard, false);
-        return removeBlockTemplate;
-    };
+		removeBlockTemplate.addEventListener('click', removeCard, false);
+		return removeBlockTemplate;
+	};
 
-    var createNewCard = function () {
-        var cardTemplate = document.createElement("div");
-        cardTemplate.className = "card";
-        cardTemplate.innerText = cards.length;
-        cardTemplate.appendChild(getRemoveBlock());
-        cards.push(cardTemplate);
+	var createNewCard = function () {
+		var cardTemplate = document.createElement('div');
+		cardTemplate.className = 'card';
+		cardTemplate.innerText = cards.length;
+		cardTemplate.appendChild(getRemoveBlock());
+		cards.push(cardTemplate);
 
-        // groupElement.childNodes = cards;
+		// groupElement.childNodes = cards;
 
-        groupElement.insertBefore(cardTemplate, addCardElement);
-    };
+		groupElement.insertBefore(cardTemplate, addCardElement);
+	};
 
-    addCardElement.addEventListener("click", createNewCard, false);
-})();
+	addCardElement.addEventListener('click', createNewCard, false);
+}());
