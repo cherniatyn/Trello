c2ba0fc55a86a70c00bc3b6a72afc2443c972cfb
cherniatyn <v.v.4ern9tin@gmail.com>
2018-08-18 13:34:45 +0300

    Added styling to Card. Added Card content.

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(concat("style.css"))
-    .pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+	return gulp.src('./sass/**/*.scss')
+		.pipe(concat("style.css"))
+		.pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./sass/**/*.scss', ['sass']);
 });
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,9 @@
 	var cards = [];
 	var groupElement = document.getElementsByClassName('group')[0];
 	var addCardElement = groupElement.children[groupElement.children.length - 1];
+	var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+		'July', 'August', 'September', 'October', 'November', 'December'];
 
 	var removeCard = function(event) {
 		return groupElement.removeChild(cards.splice(cards.indexOf(event.target.parentElement), 1)[0]);
@@ -9,18 +12,66 @@
 
 	var getRemoveBlock = function () {
 		var removeBlockTemplate = document.createElement('span');
-		removeBlockTemplate.className = 'card-remove';
+		removeBlockTemplate.classList.toggle('card-remove');
 		removeBlockTemplate.innerText = 'x';
 
 		removeBlockTemplate.addEventListener('click', removeCard, false);
 		return removeBlockTemplate;
 	};
 
+	var getDateFormat = function(date) {
+		return weekday[date.getDay()] + ', ' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' +  (date.getYear() + 1900);
+	};
+
+	var getCardContent = function () {
+		var cardContent = document.createElement('div');
+		cardContent.classList.toggle('card-content');
+
+		var progressElement = document.createElement('progress');
+		progressElement.className = 'card-content progress';
+		progressElement.max = 100;
+		progressElement.value = 10;
+		cardContent.appendChild(progressElement);
+
+		var titleElement = document.createElement('h2');
+		titleElement.className = 'card-content title';
+		titleElement.innerText = 'Title';
+
+		cardContent.appendChild(titleElement);
+
+		var cardSubContent = document.createElement('div');
+		cardSubContent.className = 'sub-content';
+
+		var moreElement = document.createElement('span');
+		moreElement.className = 'sub-content moreActions';
+		moreElement.innerText = '...';
+
+		cardSubContent.appendChild(moreElement);
+
+		var date = new Date();
+		var dateElement = document.createElement('time');
+		dateElement.className = 'sub-content time';
+		dateElement.dateTime = date;
+		dateElement.innerText = getDateFormat(date);
+
+		cardSubContent.appendChild(dateElement);
+
+		var icoElement = document.createElement('img');
+		icoElement.className = 'sub-content ico';
+		icoElement.src = 'https://www.w3schools.com/images/colorpicker.gif';
+
+		cardSubContent.appendChild(icoElement);
+
+		cardContent.appendChild(cardSubContent);
+		return cardContent;
+	};
+
 	var createNewCard = function () {
 		var cardTemplate = document.createElement('div');
-		cardTemplate.className = 'card';
-		cardTemplate.innerText = cards.length;
+		cardTemplate.classList.toggle('card');
+		// cardTemplate.innerText = cards.length;
 		cardTemplate.appendChild(getRemoveBlock());
+		cardTemplate.appendChild(getCardContent());
 		cards.push(cardTemplate);
 
 		// groupElement.childNodes = cards;
@@ -29,4 +80,9 @@
 	};
 
 	addCardElement.addEventListener('click', createNewCard, false);
+
+	window.onbeforeunload = function (e) {
+		e.preventDefault();
+		addCardElement.removeEventListener('click', createNewCard, false);
+	};
 }());
