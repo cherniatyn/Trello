ad8f70a01d548c4a95c42daea885892bdb8fc9c0
cherniatyn <v.v.4ern9tin@gmail.com>
2018-08-28 21:02:07 +0300

    include bootstrap framework

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+
+var paths = {
+	src: {
+		styles: {
+			app: 'sass/main.scss',
+			appAll: 'sass/**/*.scss',
+			libs: [
+				'./node_modules/slick-carousel/slick/slick.css'
+			]
+		},
+		scripts: {
+			appAll: [
+				'./scripts/main.js'
+			],
+			libs: [
+				'./node_modules/jquery/dist/jquery.min.js',
+				'./node_modules/slick-carousel/slick/slick.min.js',
+				'./node_modules/bootstrap/dist/js/bootstrap.min.js'
+			]
+		},
+		images: {
+			all: 'images/*'
+		}
+	}
+};
  
 gulp.task('sass', function () {
-	return gulp.src('./sass/**/*.scss')
+	return gulp.src(paths.src.styles.app)
 		.pipe(concat("style.css"))
 		.pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
